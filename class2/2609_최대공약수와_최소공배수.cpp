71a5b2b14e7345bc148a681f8f1d524280e0300a
matthewfoodsexer <matthewfoodsexer@gmail.com>
2026-04-03 00:34:28 +0900

    1018_체스판_다시_칠하기

---
+++ class2/2609_최대공약수와_최소공배수.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <numeric>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << "\n"
+         << lcm(a, b) << "\n";
+
+    return 0;
+}
