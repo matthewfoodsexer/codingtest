89f0a7783b2f5e618a3e5e87776da027fc454d85
matthewfoodsexer <matthewfoodsexer@gmail.com>
2026-04-07 00:19:11 +0900

    10828_스택

---
+++ class2/tempCodeRunnerFile.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, x;
+    string cmd;
+    cin >> n;
+
+    vector<int> stack;
+
+    for(int i = 0; i < n; i++) {
+        cin >> cmd;
+        if(cmd == "push") {
+            cin >> x;
+            stack.push_back(x);
+        } else if(cmd == "pop") {
+            if(stack.empty()) {
+                cout << -1 << "\n";
+            } else {
+                cout << stack.back() << "\n";
+                stack.pop_back();
+            }
+        } else if(cmd == "top") {
+            cout << (stack.empty() ? -1 : stack.back()) << "\n";
+        } else if(cmd == "size") {
+            cout << (stack.empty() ? 0 : stack.size()) << "\n";
+        } else if(cmd == "empty") {
+            cout << int(stack.empty()) << "\n";
+        }
+    }
+    
+    return 0;
+}
